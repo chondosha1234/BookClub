bf9351c4d8df88cef70f92d904f8a6b31d08219c
chondosha1234 <jmiller61193@gmail.com>
2023-05-14 16:00:25 -0400

    added options menu and strings

---
+++ app/src/main/java/com/chondosha/bookclub/ui/OptionsMenu.kt
@@ -0,0 +1,55 @@
+package com.chondosha.bookclub.ui
+
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.chondosha.bookclub.LocalUserRepository
+import com.chondosha.bookclub.R
+import com.chondosha.bookclub.viewmodels.UserHomeViewModel
+import com.chondosha.bookclub.viewmodels.UserHomeViewModelFactory
+import kotlinx.coroutines.launch
+
+@Composable
+fun OptionsMenu(
+    modifier: Modifier = Modifier,
+) {
+
+    val userHomeViewModel : UserHomeViewModel = viewModel(
+        factory = UserHomeViewModelFactory(LocalUserRepository.current)
+    )
+    val coroutineScope = rememberCoroutineScope()
+
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(
+        onClick = { expanded = true }
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.overflow_menu),
+            contentDescription = "Menu"
+        )
+    }
+
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false }
+    ) {
+
+        DropdownMenuItem(onClick = {
+
+        }) {
+            Text(stringResource(R.string.settings))
+        }
+
+        DropdownMenuItem(onClick = {
+            coroutineScope.launch {
+                userHomeViewModel.logout()
+            }
+        }) {
+            Text(stringResource(R.string.logout))
+        }
+    }
+}
